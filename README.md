61ea922b0b1db1d96912ad273d86c9a3d61e88f2
Moritz <fullermoritz@gmail.com>
2022-01-23 01:38:28 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# usage
+- run `poetry install` from the current directory
+- run `poetry run backup` to execute the backup functionality
+- this will execute a scheduler that updates the `registry.json` and `tokens.json` files every 60 minutes with live data from the canister
---
+++ state_backup/backup.py
@@ -0,0 +1,39 @@
+from ic.client import Client
+from ic.identity import Identity
+from ic.agent import Agent
+from ic.candid import encode, Types
+import json
+import time
+import schedule
+# params is an array, return value is encoded bytes
+params = [{'type': Types.Nat, 'value': 10}]
+data = encode(params)
+# Identity and Client are dependencies of Agent
+iden = Identity()  # creates a random keypair
+client = Client()  # creates a client to talk to the IC
+# creates an agent, combination of client and identity
+agent = Agent(iden, client)
+
+
+def main():
+    schedule.every().hour.do(backup)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+def backup():
+    print("backing up ...")
+    print(f"backup completed at {time.ctime()}")
+    # query the NFT canister
+    # doesnt change after calling `shuffleAssets`
+    tokens = agent.query_raw(
+        "pk6rk-6aaaa-aaaae-qaazq-cai", "getTokens", encode([]))[0]["value"]
+    with open('tokens.json', 'w') as f:
+        json.dump(tokens, f, ensure_ascii=False, indent=4)
+
+    # changes after every transaction
+    registry = agent.query_raw(
+        "pk6rk-6aaaa-aaaae-qaazq-cai", "getRegistry", encode([]))[0]["value"]
+    with open('registry.json', 'w') as f:
+        json.dump(registry, f, ensure_ascii=False, indent=4)
