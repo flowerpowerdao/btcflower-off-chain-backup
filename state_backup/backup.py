b0598393fdfbc73c462871b90e46298826e05121
Moritz <fullermoritz@gmail.com>
2022-03-22 22:10:46 +0100

    ✨ added disburse functionality

--- state_backup/backup.py
+++ state_backup/backup.py
@@ -1,16 +1,15 @@
-from turtle import back
 from ic.client import Client
 from ic.identity import Identity
 from ic.agent import Agent
 from ic.candid import encode, Types
-import json
 import time
 import schedule
 import sys
 
 # params is an array, return value is encoded bytes
-params = [{'type': Types.Nat, 'value': 10}]
-data = encode(params)
+# params = [{'type': Types.Nat, 'value': 10}]
+# data = encode(params)
+
 # Identity and Client are dependencies of Agent
 iden = Identity()  # creates a random keypair
 client = Client()  # creates a client to talk to the IC
@@ -20,24 +19,18 @@ agent = Agent(iden, client)
 
 def main():
     backup(canister_id=sys.argv[1])
-    schedule.every().minute.do(backup, canister_id=sys.argv[1])
+    schedule.every(5).seconds.do(backup, canister_id=sys.argv[1])
 
     while True:
         schedule.run_pending()
         time.sleep(1)
 
+
 def backup(canister_id):
-    print("backing up ...")
-    print(f"backup completed at {time.ctime()}")
+    print(f"disburse completed at {time.ctime()}")
     # query the NFT canister
     # doesnt change after calling `shuffleAssets`
-    tokens = agent.query_raw(
-        canister_id, "getTokens", encode([]))[0]["value"]
-    with open('tokens.json', 'w') as f:
-        json.dump(tokens, f, ensure_ascii=False, indent=4)
-
-    # changes after every transaction
-    registry = agent.query_raw(
-        canister_id, "getRegistry", encode([]))[0]["value"]
-    with open('registry.json', 'w') as f:
-        json.dump(registry, f, ensure_ascii=False, indent=4)
+    result = agent.update_raw(
+        canister_id, "disburse", encode([]))
+        
+    print(result) 
