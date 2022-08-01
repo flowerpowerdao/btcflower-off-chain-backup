20e1da97c5e821b95eceb3db1d6df077fd5a595c
Moritz <fullermoritz@gmail.com>
2022-08-01 17:05:52 +0200

    ✨ add update raw

--- cron/cron.py
+++ cron/cron.py
@@ -19,16 +19,21 @@ s = sched.scheduler(time.time, time.sleep)
 def cron(canister_id, interval):
     try:
         print(f"disburse completed at {time.ctime()}")
-        response = agent.query_raw(
-            canister_id, '__get_candid_interface_tmp_hack', encode([]))
-        canister_did = response[0]['value']  # type: ignore
-        my_canister = Canister(
-            agent=agent, canister_id=canister_id, candid=canister_did)
-        # query the NFT canister
-        # doesnt change after calling `shuffleAssets`
-        my_canister.cronSalesSettlements()  # type: ignore
-        my_canister.cronDisbursements()  # type: ignore
-        my_canister.cronSettlements()  # type: ignore
+        # response = agent.query_raw(
+        #     canister_id, '__get_candid_interface_tmp_hack', encode([]))
+        # canister_did = response[0]['value']  # type: ignore
+        # my_canister = Canister(
+        #     agent=agent, canister_id=canister_id, candid=canister_did)
+        # # query the NFT canister
+        # # doesnt change after calling `shuffleAssets`
+        # print(my_canister.cronSalesSettlements())  # type: ignore
+        # print(my_canister.cronDisbursements())  # type: ignore
+        # print(my_canister.cronSettlements())  # type: ignore
+
+        print("hi")
+        canister_id = 'skjpp-haaaa-aaaae-qac7q-cai'
+        result = agent.update_raw(canister_id, 'cronDisbursements', encode([]))
+        print(result)
     except Exception as e:
         print(e)
 
